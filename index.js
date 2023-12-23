ae9991a337a3fd2d9bc45603a7c541d899dd7b87
Yash Pandav <145275579+yashpandav@users.noreply.github.com>
2023-12-23 23:13:40 +0530

    23 / 12 / 2023

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let mode = document.getElementById("mode");
 let logo = document.getElementById("logoIMG");
 let navWhite = document.querySelector("nav");
 let needWhite = document.querySelector(".second-main h1");
-let cardP = document.querySelector(".card p");
+let cardP = document.querySelectorAll(".card p");
 let slogan = document.querySelector("#fitness h4");
 let click = false;
 
@@ -14,7 +14,9 @@ mode.addEventListener("click", () => {
           logo.src = "logo/white-logo.png";
           needWhite.classList.remove("need-black");
           needWhite.classList.add("need-white");
-          cardP.style.color = "#100F11";
+          for(let i = 0; i < cardP.length; i++){
+               cardP[i].style.color = "#100F11";
+          }
           slogan.style.color = "#321F44";
           click = true;
           active();
@@ -25,12 +27,15 @@ mode.addEventListener("click", () => {
           logo.src = "logo/dark-logo.png"
           needWhite.classList.remove("need-white");
           needWhite.classList.add("need-black");
-          cardP.style.color = "rgb(178, 190, 202)";
+          for(let i = 0; i < cardP.length; i++){
+               cardP[i].style.color = "rgb(178, 190, 202)";
+          }
           slogan.style.color = "orange";
           click = false;
           active();
      }
 });
+
 
 function active(){
      let navLinks = document.querySelectorAll("nav a");
@@ -47,6 +52,58 @@ function active(){
      }
 }
 window.addEventListener("scroll", active);
+
+let mainGym = document.querySelectorAll(".gym-intro");
+for(let gym of mainGym){
+     gym.addEventListener("mouseover" , ()=>{
+               let introGym = document.querySelector(`#${gym.id} h4`);
+               let gymIcon = document.querySelector(`#${gym.id} i`);
+               let outroGym = document.querySelector(`#${gym.id} div`);
+               if(gymIcon.id == "first"){
+                    gymIcon.style.setProperty("color", "red");
+                    outroGym.style.backgroundColor = "red";
+                    introGym.style.backgroundColor = "red";
+                    introGym.style.color = "whitesmoke";
+               }
+               else if(gymIcon.id == "sec"){
+                    gymIcon.style.setProperty("color", "rgb(0, 13, 255)");
+                    outroGym.style.backgroundColor = "rgb(0, 13, 255)";
+                    introGym.style.backgroundColor = "rgb(0, 13, 255)";
+                    introGym.style.color = "whitesmoke";
+               }
+               else{
+                    gymIcon.style.setProperty("color", "green");
+                    outroGym.style.backgroundColor = "green";
+                    introGym.style.backgroundColor = "green";
+                    introGym.style.color = "whitesmoke";
+               }
+               introGym.classList.add("aniForGymIntro");    
+               introGym.classList.remove("visibilityForIntro");    
+               introGym.classList.add("visible");
+
+               outroGym.classList.add("aniForGymInfo");
+               outroGym.classList.remove("visibilityForIntro");
+               outroGym.classList.add("visible");
+     })
+     gym.addEventListener("mouseout" , ()=>{
+               let introGym = document.querySelector(`#${gym.id} h4`);
+               let outroGym = document.querySelector(`#${gym.id} div`);
+               let gymIcon = document.querySelector(`#${gym.id} i`);
+               gymIcon.style.removeProperty("color");
+
+               introGym.style.setProperty("visibility" , "hidden");
+               introGym.classList.remove("aniForGymIntro"); 
+               introGym.classList.add("visibilityForIntro");
+               introGym.classList.remove("visible");    
+             
+               outroGym.style.setProperty("visibility" , "hidden");
+               outroGym.classList.remove("aniForGymInfo"); 
+               outroGym.classList.add("visibilityForIntro");
+               outroGym.classList.remove("visible");    
+     })
+}
+
+
 
 //#969c3a  :: GOLDEN
 //#6b3a9c  :: PURPLE
