ae71859ae72b59d15028aa3821feac8121def4bc
Yash Pandav <145275579+yashpandav@users.noreply.github.com>
2023-12-23 12:57:44 +0530

    changes with carousel

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let mode = document.getElementById("mode");
 let logo = document.getElementById("logoIMG");
-let mainIMG = document.getElementById("backImg");
 let navWhite = document.querySelector("nav");
+let needWhite = document.querySelector(".second-main h1");
+let cardP = document.querySelector(".card p");
 
 let click = false;
 
@@ -11,20 +12,39 @@ mode.addEventListener("click", () => {
           navWhite.classList.add("nav-white");
           document.body.style.backgroundColor = "#EAE1EB";
           logo.src = "logo/white-logo.png";
-          mainIMG.src = "logo/main-white.png";
+          needWhite.classList.remove("need-black");
+          needWhite.classList.add("need-white");
+          cardP.style.color = "#100F11"
           click = true;
      } else {
           document.body.style.backgroundColor = "#100F11";
           navWhite.classList.remove("nav-white");
           navWhite.classList.add("nav-dark");
           logo.src = "logo/dark-logo.png"
-          mainIMG.src = "logo/main-black.png";
+          needWhite.classList.remove("need-white");
+          needWhite.classList.add("need-black");
+          cardP.style.color = "rgb(178, 190, 202)";
           click = false;
      }
 });
 
+let navLinks = document.querySelectorAll("nav a");
+window.addEventListener("scroll", ()=>{
+     let navLinks = document.querySelectorAll("nav a");
+     if(navLinks[1].classList.contains("active")){
+          navWhite.style.backgroundColor = "transparent";
+     }
+     else{
+          if(click === false){
+               navWhite.style.backgroundColor = "#100F11";
+          }
+          else{
+               navWhite.style.backgroundColor = "#EAE1EB";
+          }
+
+     }
+})
+
 //#969c3a  :: GOLDEN
 //#6b3a9c  :: PURPLE
 //#321F44  :: purple REQUIRED
-
-
